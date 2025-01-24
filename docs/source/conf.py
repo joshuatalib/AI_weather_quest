8252ae74e9af7c618aca71ebcd8de847012ee692
joshuatalib <joshua.talib@ecmwf.int>
2025-01-24 11:10:22 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'AI_Weather_Quest'
+copyright = '2025, Talib'
+author = 'Joshua Talib'
 
 release = '0.1'
 version = '0.1.0'
