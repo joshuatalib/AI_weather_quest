385f922ce7a53be3ad905ee86e382210fb89b5bd
joshuatalib <joshua.talib@ecmwf.int>
2025-02-05 15:00:51 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-html_logo = os.path.abspath("docs/source/Logo_AIWQ_Dark.png")
+html_logo = os.path.abspath("Logo_AIWQ_Dark.png")
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
