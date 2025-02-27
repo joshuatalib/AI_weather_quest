4de5185779e9602008b0ec1db078fa334f7ad2b2
joshuatalib <joshua.talib@ecmwf.int>
2025-02-27 12:07:53 +0000

    Update conf.py for logo2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-html_logo = os.path.abspath("Logo.png")
+html_logo = os.path.abspath("Logo2.png")
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
